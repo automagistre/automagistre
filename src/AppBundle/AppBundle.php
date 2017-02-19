ab28c64682abf247b115b71e83d2efb86f61f142
Konstantin Grachev <me@grachevko.ru>
2017-02-19 13:50:40 +0300

    Add Uuid

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,8 +2,26 @@
 
 namespace AppBundle;
 
+use Ramsey\Uuid\Builder\DefaultUuidBuilder;
+use Ramsey\Uuid\Codec\OrderedTimeCodec;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $uuidFactory = new UuidFactory();
+        $uuidBuilder = new DefaultUuidBuilder($uuidFactory->getNumberConverter());
+        $uuidFactory->setUuidBuilder($uuidBuilder);
+        $uuidFactory->setCodec(new OrderedTimeCodec($uuidBuilder));
+
+        Uuid::setFactory($uuidFactory);
+    }
 }
---
+++ src/AppBundle/Doctrine/UuidGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Id\AbstractIdGenerator;
+use Doctrine\ORM\Mapping\Entity;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class UuidGenerator extends AbstractIdGenerator
+{
+    /**
+     * Generate an identifier.
+     *
+     * @param EntityManager $em
+     * @param Entity        $entity
+     *
+     * @return UuidInterface
+     */
+    public function generate(EntityManager $em, $entity)
+    {
+        return Uuid::uuid1();
+    }
+}
