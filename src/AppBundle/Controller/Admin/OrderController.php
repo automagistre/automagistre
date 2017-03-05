47fc7c28859f293fb2827bd865d55a94e49ae6f9
Konstantin Grachev <me@grachevko.ru>
2017-03-05 22:16:07 +0300

    Order search upgrade

--- src/AppBundle/Controller/Admin/OrderController.php
+++ src/AppBundle/Controller/Admin/OrderController.php
@@ -18,20 +18,32 @@ final class OrderController extends AdminController
         $sortDirection = null,
         $dqlFilter = null
     ) {
-        return $this->em->getRepository(Order::class)->createQueryBuilder('orders')
+        $qb = $this->em->getRepository(Order::class)->createQueryBuilder('orders')
             ->leftJoin('orders.client', 'client')
             ->leftJoin('client.person', 'person')
             ->leftJoin('orders.car', 'car')
             ->leftJoin('car.carModel', 'carModel')
             ->leftJoin('car.carModification', 'carModification')
-            ->leftJoin('carModel.manufacturer', 'manufacturer')
-            ->where('person.firstname LIKE :search')
-            ->orWhere('person.lastname LIKE :search')
-            ->orWhere('car.gosnomer LIKE :search')
-            ->orWhere('carModel.name LIKE :search')
-            ->orWhere('carModification.name LIKE :search')
-            ->orWhere('manufacturer.name LIKE :search')
-            ->setParameter('search', '%'.$searchQuery.'%');
+            ->leftJoin('carModel.manufacturer', 'manufacturer');
+
+        foreach (explode(' ', $searchQuery) as $key => $item) {
+            $key = ':search_'.$key;
+
+            $qb->andWhere($qb->expr()->orX(
+                $qb->expr()->like('person.firstname', $key),
+                $qb->expr()->like('person.lastname', $key),
+                $qb->expr()->like('person.telephone', $key),
+                $qb->expr()->like('person.email', $key),
+                $qb->expr()->like('car.gosnomer', $key),
+                $qb->expr()->like('carModel.name', $key),
+                $qb->expr()->like('carModification.name', $key),
+                $qb->expr()->like('manufacturer.name', $key)
+            ));
+
+            $qb->setParameter($key, '%'.$item.'%');
+        }
+
+        return $qb;
     }
 
     public function isActionAllowed($actionName): bool
