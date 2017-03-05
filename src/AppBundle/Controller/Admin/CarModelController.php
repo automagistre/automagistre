4eb8a19f14c980527394207395dd2cbed61dddc7
Konstantin Grachev <me@grachevko.ru>
2017-03-06 00:12:37 +0300

    Car dictionary search

---
+++ src/AppBundle/Controller/Admin/CarModelController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\CarModification;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class CarModelController extends AdminController
+{
+    protected function createSearchQueryBuilder(
+        $entityClass,
+        $searchQuery,
+        array $searchableFields,
+        $sortField = null,
+        $sortDirection = null,
+        $dqlFilter = null
+    ) {
+        $qb = $this->em->getRepository(CarModification::class)->createQueryBuilder('modification')
+            ->leftJoin('modification.carGeneration', 'generation')
+            ->leftJoin('generation.carModel', 'model')
+            ->leftJoin('model.manufacturer', 'manufacturer');
+
+        foreach (explode(' ', $searchQuery) as $key => $item) {
+            $key = ':search_'.$key;
+
+            $qb->andWhere($qb->expr()->orX(
+                $qb->expr()->like('modification.name', $key),
+                $qb->expr()->like('generation.name', $key),
+                $qb->expr()->like('model.name', $key),
+                $qb->expr()->like('manufacturer.name', $key)
+            ));
+
+            $qb->setParameter($key, '%'.$item.'%');
+        }
+
+        return $qb;
+    }
+}
