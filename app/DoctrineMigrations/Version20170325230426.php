48161b1555b905f26d748d4aafef7f6271c1d612
Konstantin Grachev <me@grachevko.ru>
2017-03-26 02:05:32 +0300

    Add fake part to make reference

---
+++ app/DoctrineMigrations/Version20170325230426.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170325230426 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('INSERT INTO part (name, manufacturer_id, number, reserved) VALUES (\'ЗДЕСЬ БЫЛА ЗАПЧАСТЬ КОТОРУЮ УДАЛИЛИ\', 1, \'----------\', 0)');
+
+        $this->addSql('UPDATE order_part SET part_id = (SELECT id FROM part WHERE part.number = \'----------\') WHERE part_id IS NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
