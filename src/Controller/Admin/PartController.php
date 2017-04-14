86bfb4164b429e314b54018cd82f1d61496cc92d
Konstantin Grachev <me@grachevko.ru>
2017-04-14 13:07:33 +0300

    Move admin routes under EasyAdminController. Fix routing.

--- src/Controller/Admin/PartController.php
+++ src/Controller/Admin/PartController.php
@@ -2,11 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Manufacturer;
+use App\Entity\Motion;
 use App\Entity\Part;
+use App\Model\Part as PartModel;
+use App\Model\WarehousePart;
+use App\Part\Finder;
+use Doctrine\ORM\Query\Expr\Join;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController;
 use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
@@ -14,13 +22,19 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 final class PartController extends AdminController
 {
     /**
+     * @var Finder
+     */
+    private $finder;
+
+    /**
      * @var EventDispatcherInterface
      */
     private $dispatcher;
 
-    public function __construct(EventDispatcherInterface $dispatcher)
+    public function __construct(EventDispatcherInterface $dispatcher, Finder $finder)
     {
         $this->dispatcher = $dispatcher;
+        $this->finder = $finder;
     }
 
     protected function newAction()
@@ -98,5 +112,63 @@ final class PartController extends AdminController
         }, (array) $paginator->getCurrentPageResults());
 
         return $this->json(['results' => $data]);
+    }
+
+    public function numberSearchAction()
+    {
+        if (!$number = $this->request->query->get('number')) {
+            throw new BadRequestHttpException();
+        }
+
+        $manufacturerRepository = $this->em->getRepository(Manufacturer::class);
+
+        $parts = $this->finder->search($number);
+
+        if (!$parts) {
+            return new Response('', Response::HTTP_NO_CONTENT);
+        }
+
+        return $this->json(array_map(function (PartModel $model) use ($manufacturerRepository) {
+            $manufacturer = $manufacturerRepository->findOneBy(['name' => $model->manufacturer]);
+            if (!$manufacturer) {
+                $manufacturer = new Manufacturer();
+                $manufacturer->setName($model->manufacturer);
+                $this->em->persist($manufacturer);
+            }
+
+            return [
+                'manufacturer' => [
+                    'id'   => $manufacturer->getId(),
+                    'name' => $manufacturer->getName(),
+                ],
+                'name'         => $model->name,
+                'number'       => $model->number,
+            ];
+        }, array_filter($parts, function (PartModel $model) use ($number) {
+            return false !== strpos($model->number, $number);
+        })));
+    }
+
+    public function stockAction()
+    {
+        $request = $this->request;
+        $qb = $this->em->getRepository(Part::class)->createQueryBuilder('part')
+            ->addSelect('SUM(motion.quantity) AS quantity')
+            ->leftJoin(Motion::class, 'motion', Join::WITH, 'part.id = motion.part')
+            ->groupBy('part.id')
+            ->having('SUM(motion.quantity) <> 0');
+
+        $paginator = $this->get('easyadmin.paginator')->createOrmPaginator($qb, $request->query->getInt('page', 1), 20);
+
+        $parts = array_map(function (array $data) {
+            return new WarehousePart([
+                'part'     => $data[0],
+                'quantity' => $data['quantity'],
+            ]);
+        }, (array) $paginator->getCurrentPageResults());
+
+        return $this->render('easy_admin/warehouse/list.html.twig', [
+            'parts' => $parts,
+        ]);
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,25 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Manufacturer;
-use App\Model\Part;
-use App\Part\Finder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @var Finder
-     */
-    private $finder;
-
-    public function __construct(Finder $finder)
-    {
-        $this->finder = $finder;
-    }
-
     /**
      * @Route("/", name="homepage")
      */
@@ -30,40 +16,5 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => dirname($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
-    }
-
-    /**
-     * @Route("/part/search/{number}", name="part-search")
-     */
-    public function searchPartAction(string $number)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $manufacturerRepository = $em->getRepository(Manufacturer::class);
-
-        $parts = $this->finder->search($number);
-
-        if (!$parts) {
-            return new Response('', Response::HTTP_NO_CONTENT);
-        }
-
-        return $this->json(array_map(function (Part $model) use ($em, $manufacturerRepository) {
-            $manufacturer = $manufacturerRepository->findOneBy(['name' => $model->manufacturer]);
-            if (!$manufacturer) {
-                $manufacturer = new Manufacturer();
-                $manufacturer->setName($model->manufacturer);
-                $em->persist($manufacturer);
-            }
-
-            return [
-                'manufacturer' => [
-                    'id'   => $manufacturer->getId(),
-                    'name' => $manufacturer->getName(),
-                ],
-                'name'         => $model->name,
-                'number'       => $model->number,
-            ];
-        }, array_filter($parts, function (Part $model) use ($number) {
-            return false !== strpos($model->number, $number);
-        })));
     }
 }
