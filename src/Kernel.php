354578979e7a15de80638c103690de628273f32d
Konstantin Grachev <me@grachevko.ru>
2017-04-15 21:23:23 +0300

    declare(strict_types=1) to src folder

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Ramsey\Uuid\Builder\DefaultUuidBuilder;
--- src/Paginator/Paginator.php
+++ src/Paginator/Paginator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Paginator;
 
 use Pagerfanta\Adapter\DoctrineORMAdapter;
