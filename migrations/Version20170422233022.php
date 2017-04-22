9364cd477eea8c72b8ff7818172af00f2399c413
Konstantin Grachev <me@grachevko.ru>
2017-04-23 02:35:58 +0300

    Fix CS

--- migrations/Version20170422233022.php
+++ migrations/Version20170422233022.php
@@ -32,6 +32,5 @@ class Version20170422233022 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-
     }
 }
