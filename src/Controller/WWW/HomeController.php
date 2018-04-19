45afdfe57babd10bdeafae8aee0be44908f3aa03
Konstantin Grachev <me@grachevko.ru>
2018-04-19 11:50:01 +0300

    [WWW] All main pages

--- src/Controller/WWW/HomeController.php
+++ src/Controller/WWW/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\WWW;
 
+use LogicException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,5 +39,65 @@ final class HomeController extends Controller
     {
         return $this->render('www/repair.html.twig', [
         ]);
+    }
+
+    /**
+     * @Route("/diagnostics/{type}",
+     *     name="www_diagnostics",
+     *     defaults={"type": "free"},
+     *     requirements={"type": "free|comp"}
+     * )
+     */
+    public function diagnostics(string $type): Response
+    {
+        if ('comp' === $type) {
+            return $this->render('www/diagnostics_comp.html.twig');
+        }
+
+        if ('free' === $type) {
+            return $this->render('www/diagnostics_free.html.twig');
+        }
+
+        throw new LogicException('Unreachable statement');
+    }
+
+    /**
+     * @Route("/tire", name="www_type")
+     */
+    public function tire(): Response
+    {
+        return $this->render('www/tire_service.html.twig');
+    }
+
+    /**
+     * @Route("/brands", name="www_brands")
+     */
+    public function brands(): Response
+    {
+        return $this->render('www/brands.html.twig');
+    }
+
+    /**
+     * @Route("/corporates", name="www_corporates")
+     */
+    public function corporates()
+    {
+        return $this->render('www/corporates.html.twig');
+    }
+
+    /**
+     * @Route("/price-list", name="www_price-list")
+     */
+    public function priceList(): Response
+    {
+        return $this->render('www/price_list.html.twig');
+    }
+
+    /**
+     * @Route("/maintenance", name="www_maintenance")
+     */
+    public function maintenance(): Response
+    {
+        return $this->render('www/maintenance.html.twig');
     }
 }
---
+++ tests/Controller/WWW/SmokeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Controller\WWW;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class SmokeTest extends WebTestCase
+{
+    /**
+     * @dataProvider pagesProvider
+     */
+    public function testPages(string $url, int $statusCode): void
+    {
+        $client = self::createClient();
+        $client->setServerParameter('HTTP_HOST', 'www.automagistre.ru');
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+
+        $this->assertSame($statusCode, $response->getStatusCode());
+    }
+
+    public function pagesProvider()
+    {
+        yield ['/', 200];
+        yield ['/home', 200];
+        yield ['/repair', 200];
+        yield ['/diagnostics/free', 200];
+        yield ['/diagnostics/comp', 200];
+        yield ['/tire', 200];
+        yield ['/brands', 200];
+        yield ['/corporates', 200];
+        yield ['/price-list', 200];
+        yield ['/maintenance', 200];
+    }
+}
