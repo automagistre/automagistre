5018146423ae1c357433af8ca25907d856461016
Konstantin Grachev <me@grachevko.ru>
2018-06-02 21:45:00 +0300

    Composer update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => dirname($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => dirname($this->getParameter('kernel.root_dir').'/..').\DIRECTORY_SEPARATOR,
         ]);
     }
 }
