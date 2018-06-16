0cdad6f026a57306bcc5be61b9f77fc6e1608541
Konstantin Grachev <me@grachevko.ru>
2018-06-16 15:36:02 +0300

    [Refactoring] Move admin routes under admin_ prefix

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
@@ -18,12 +19,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 final class SecurityController extends Controller
 {
     /**
-     * @Route("/login", name="admin_login")
+     * @Route("/login", name="login")
+     * @Method("GET")
      */
     public function loginAction(FormFactoryInterface $formFactory, AuthenticationUtils $authUtils): Response
     {
         $form = $formFactory->createNamedBuilder('', FormType::class, null, [
-            'action' => $this->generateUrl('login_check'),
+            'action' => $this->generateUrl('admin_login_check'),
             'csrf_field_name' => '_csrf_token',
             'csrf_token_id' => 'authenticate',
         ])
