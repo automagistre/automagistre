f6b24f9406873ce05aad401f0328ef7b86caab5f
Konstantin Grachev <me@grachevko.ru>
2018-08-12 11:09:04 +0300

    [Security] Restrict any actions on OrderItem if order is not editable

--- src/Controller/EasyAdmin/OrderItemController.php
+++ src/Controller/EasyAdmin/OrderItemController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\EasyAdmin;
 
 use App\Entity\Order;
+use LogicException;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -12,6 +13,23 @@ use Symfony\Component\HttpFoundation\Response;
  */
 abstract class OrderItemController extends AbstractController
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function isActionAllowed($actionName): bool
+    {
+        $order = $this->getEntity(Order::class);
+        if (!$order instanceof Order) {
+            throw new LogicException('Order required.');
+        }
+
+        if (!$order->isEditable()) {
+            return false;
+        }
+
+        return parent::isActionAllowed($actionName);
+    }
+
     /**
      * {@inheritdoc}
      */
