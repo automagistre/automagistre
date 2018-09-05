7849d19c26acbb864181ddaf585cb8e6b34eaebb
Konstantin Grachev <me@grachevko.ru>
2018-09-05 20:31:57 +0300

    [Feature] Allow create payment

--- src/Controller/EasyAdmin/PaymentController.php
+++ src/Controller/EasyAdmin/PaymentController.php
@@ -5,13 +5,44 @@ declare(strict_types=1);
 namespace App\Controller\EasyAdmin;
 
 use App\Entity\Operand;
+use App\Manager\PaymentManager;
 use Doctrine\ORM\QueryBuilder;
+use stdClass;
 
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
  */
 final class PaymentController extends AbstractController
 {
+    /**
+     * @var PaymentManager
+     */
+    private $paymentManager;
+
+    public function __construct(PaymentManager $paymentManager)
+    {
+        $this->paymentManager = $paymentManager;
+    }
+
+    protected function createNewEntity(): stdClass
+    {
+        $obj = new stdClass();
+        $obj->id = null;
+        $obj->recipient = $this->getEntity(Operand::class);
+        $obj->description = null;
+        $obj->amount = null;
+
+        return $obj;
+    }
+
+    /**
+     * @param stdClass $entity
+     */
+    protected function persistEntity($entity): void
+    {
+        $this->paymentManager->createPayment($entity->recipient, $entity->description, $entity->amount);
+    }
+
     /**
      * {@inheritdoc}
      */
