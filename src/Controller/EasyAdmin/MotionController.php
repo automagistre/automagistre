b0e0b91fe6cabc394767c0d7960863f843ea5725
Konstantin Grachev <me@grachevko.ru>
2018-09-15 17:42:02 +0300

    [Feature] Filter motion by income and outcome

---
+++ src/Controller/EasyAdmin/MotionController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\EasyAdmin;
+
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class MotionController extends AbstractController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function createListQueryBuilder(
+        $entityClass,
+        $sortDirection,
+        $sortField = null,
+        $dqlFilter = null
+    ): QueryBuilder
+    {
+        $qb = parent::createListQueryBuilder($entityClass, $sortDirection, $sortField, $dqlFilter);
+
+        $request = $this->request;
+
+        $filter = $request->query->get('filter');
+
+        if ('income' === $filter) {
+            $qb->andWhere('entity.quantity >= 0');
+        } elseif ('outcome' === $filter) {
+            $qb->andWhere('entity.quantity <= 0');
+        }
+
+        return $qb;
+    }
+}
