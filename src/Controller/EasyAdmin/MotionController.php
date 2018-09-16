7ec060f9a93addf2258aad731b87d898516e52e4
Konstantin Grachev <me@grachevko.ru>
2018-09-16 22:47:09 +0300

    [Enhancement] Filter motion by part. #96

--- src/Controller/EasyAdmin/MotionController.php
+++ src/Controller/EasyAdmin/MotionController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\EasyAdmin;
 
+use App\Entity\Part;
 use Doctrine\ORM\QueryBuilder;
 
 /**
@@ -30,6 +31,12 @@ final class MotionController extends AbstractController
             $qb->andWhere('entity.quantity >= 0');
         } elseif ('outcome' === $filter) {
             $qb->andWhere('entity.quantity <= 0');
+        }
+
+        $part = $this->getEntity(Part::class);
+        if ($part instanceof Part) {
+            $qb->andWhere('entity.part = :part')
+                ->setParameter('part', $part);
         }
 
         return $qb;
