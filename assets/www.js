99b9102191af8bb0ffe2f7dee014e22609885c31
Konstantin Grachev <me@grachevko.ru>
2018-11-11 19:00:01 +0300

    In progress

---
+++ assets/www.js
@@ -0,0 +1,33 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+// require('../less/main.less');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+
+require('bootstrap-datepicker');
+require('fotorama/fotorama');
+require('gsap');
+require('jquery');
+require('jquery-scrollify');
+require('jquery-validation');
+require('jquery-viewport-checker');
+require('jquery.maskedinput/src/jquery.maskedinput');
+require('jquery.scroolly');
+require('lazysizes');
+require('malihu-custom-scrollbar-plugin');
+require('noty');
+require('odometer');
+require('rename-css-selectors');
+require('salvattore');
+require('slick-carousel');
+require('velocity-animate');
+require('zoom.js');
+
+require('./js/helpers/transition');
+require('./js/main.js');
