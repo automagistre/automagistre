bac8673f7cecf5ef5d18f6e3d1d2bd21ac3bc368
Konstantin Grachev <me@grachevko.ru>
2018-12-20 19:08:51 +0300

    Composer update (#154)

--- src/Controller/Admin/PartExplorerController.php
+++ src/Controller/Admin/PartExplorerController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
  */
-final class PartExplorerController extends Controller
+final class PartExplorerController extends AbstractController
 {
     /**
      * @Route("/part-explorer/{partnumber}", name="part_explorer")
