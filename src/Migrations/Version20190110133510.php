5fdfac4ec1e4552e3a4d88fef2524985ceac0f64
Konstantin Grachev <me@grachevko.ru>
2019-01-10 16:37:26 +0300

    [Bugfix] CarRecommendation::$tenant can be null

---
+++ src/Migrations/Version20190110133510.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190110133510 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->skipIf('landlord' !== $this->connection->getDatabase(), 'Landlord only');
+
+        $this->addSql('ALTER TABLE car_recommendation CHANGE realization_tenant realization_tenant SMALLINT DEFAULT NULL COMMENT \'(DC2Type:tenant_enum)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->skipIf('landlord' !== $this->connection->getDatabase(), 'Landlord only');
+
+        $this->addSql('ALTER TABLE car_recommendation CHANGE realization_tenant realization_tenant SMALLINT NOT NULL COMMENT \'(DC2Type:tenant)\'');
+    }
+}
