797bfca897fcddda0ae17424b50026926c2a4702
Konstantin Grachev <me@grachevko.ru>
2019-01-12 22:25:24 +0300

    [Feature] Tenant have different requisites

--- src/Enum/Tenant.php
+++ src/Enum/Tenant.php
@@ -7,6 +7,7 @@ namespace App\Enum;
 use Grachevko\Enum\Enum;
 
 /**
+ * @method bool isSandbox()
  * @method string getDisplayName()
  * @method static self msk()
  * @method static self fromIdentifier(string $name)
@@ -16,7 +17,9 @@ use Grachevko\Enum\Enum;
 final class Tenant extends Enum
 {
     private const SANDBOX = 0;
+
     private const MSK = 1;
+
     private const KAZAN = 2;
 
     /**
@@ -37,9 +40,51 @@ final class Tenant extends Enum
         self::KAZAN => 'Казань',
     ];
 
+    /**
+     * @var array
+     */
+    protected static $requisites = [
+        self::SANDBOX => [],
+        self::MSK => [
+            'name' => 'ООО "Автомагистр"',
+            'address' => 'г.Москва, ул.Люблинская, д.31/1',
+            'telephones' => [
+                '+7 (495) 984-81-82',
+                '+7 (985) 929-40-87',
+            ],
+            'ogrn' => '5137746189060',
+            'inn' => '7725812690',
+            'kpp' => '772301001',
+            'rs' => '40702810202000000371',
+            'ks' => '30101810800000000427',
+            'bik' => '044583427',
+        ],
+        self::KAZAN => [
+            'name' => 'ИП Ахметзянов А.А.',
+            'address' => 'г. Казань, Магистральная 33 к.1',
+            'telephones' => [
+                '+7 927 244-48-68',
+            ],
+            'ogrn' => '318169000126792',
+            'inn' => '166017663015',
+            'rs' => '40802810500000686477',
+            'ks' => '30101810145250000974',
+            'bik' => '044583427',
+        ],
+    ];
+
     public function getIdentifier(): string
     {
         return self::$identifier[$this->getId()];
+    }
+
+    public function getRequisites(): array
+    {
+        if ($this->isSandbox()) {
+            return self::$requisites[self::MSK];
+        }
+
+        return self::$requisites[$this->getId()];
     }
 
     public static function isValid(string $identifier): bool
