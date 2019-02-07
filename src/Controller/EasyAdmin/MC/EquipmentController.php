94f875be48f9026d2934531c4eed584a1bffc966
Konstantin Grachev <me@grachevko.ru>
2019-02-07 18:14:07 +0300

    [Enhancement] Search in MC\Equipment

--- src/Controller/EasyAdmin/MC/EquipmentController.php
+++ src/Controller/EasyAdmin/MC/EquipmentController.php
@@ -6,6 +6,7 @@ namespace App\Controller\EasyAdmin\MC;
 
 use App\Controller\EasyAdmin\AbstractController;
 use App\Entity\Landlord\MC\Equipment;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
@@ -20,5 +21,43 @@ final class EquipmentController extends AbstractController
         parent::persistEntity($entity);
 
         $this->setReferer($this->generateEasyPath($entity, 'show'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createSearchQueryBuilder(
+        $entityClass,
+        $searchQuery,
+        array $searchableFields,
+        $sortField = null,
+        $sortDirection = null,
+        $dqlFilter = null
+    ): QueryBuilder {
+        $qb = $this->registry->repository(Equipment::class)->createQueryBuilder('entity');
+
+        if ('' === $searchQuery) {
+            return $qb;
+        }
+
+        $qb
+            ->leftJoin('entity.model', 'carModel')
+            ->leftJoin('carModel.manufacturer', 'manufacturer');
+
+        foreach (\explode(' ', $searchQuery) as $key => $searchString) {
+            $key = ':search_'.$key;
+
+            $qb->andWhere($qb->expr()->orX(
+                $qb->expr()->like('entity.equipment.engine.name', $key),
+                $qb->expr()->like('carModel.name', $key),
+                $qb->expr()->like('carModel.localizedName', $key),
+                $qb->expr()->like('manufacturer.name', $key),
+                $qb->expr()->like('manufacturer.localizedName', $key)
+            ));
+
+            $qb->setParameter($key, '%'.$searchString.'%');
+        }
+
+        return $qb;
     }
 }
