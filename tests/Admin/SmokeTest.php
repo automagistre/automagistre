b45f437563c40b9569d4a6b1f4f8deeb707e9d60
Konstantin Grachev <me@grachevko.ru>
2019-05-19 18:09:24 +0300

    [PHPUnit] Add test for admin /login page

---
+++ tests/Admin/SmokeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class SmokeTest extends WebTestCase
+{
+    /**
+     * @dataProvider pages
+     */
+    public function testSmoke(string $url, int $statusCode): void
+    {
+        $client = self::createClient();
+        $client->setServerParameter('HTTP_HOST', 'sto.automagistre.ru');
+
+        $client->request('GET', $url);
+        $response = $client->getResponse();
+
+        static::assertSame($statusCode, $response->getStatusCode());
+    }
+
+    public function pages(): \Generator
+    {
+        yield ['/login', 200];
+    }
+}
