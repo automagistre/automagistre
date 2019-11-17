04e7ed51c2ae33d2f283deb811c4b8321993af7c
Konstantin Grachev <me@grachevko.ru>
2019-11-18 00:00:33 +0300

    cs fix

--- src/Kernel.php
+++ src/Kernel.php
@@ -32,7 +32,7 @@ final class Kernel extends SymfonyKernel
         foreach ((array) $contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->getEnvironment()])) {
                 if (!\class_exists($class)) {
-                    throw new \LogicException(sprintf('Class %s not exists', $class));
+                    throw new \LogicException(\sprintf('Class %s not exists', $class));
                 }
 
                 \assert(\is_subclass_of($class, BundleInterface::class));
