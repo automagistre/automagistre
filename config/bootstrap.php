8c3c54415a321e19121fe54cd6679aea3f621e33
Konstantin Grachev <me@grachevko.ru>
2019-11-17 23:00:16 +0300

    Load .env only if file exist

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,17 +4,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
 
-// Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (\is_array($env = @include \dirname(__DIR__).'/.env.local')) {
-    foreach ($env as $k => $v) {
-        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== \strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
-    }
-} elseif (!\class_exists(Dotenv::class)) {
-    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
-} else {
-    // load all the .env files
-    (new Dotenv(false))->loadEnv(\dirname(__DIR__).'/.env');
+if (\class_exists(Dotenv::class) && \file_exists($path = \dirname(__DIR__).'/.env')) {
+    (new Dotenv(false))->loadEnv($path);
 }
 
 $_SERVER += $_ENV;
