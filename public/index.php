2c7344dd121f733da421c2a19edf66ab564269cb
Konstantin Grachev <me@grachevko.ru>
2019-11-17 22:31:13 +0300

    Add bootstrap.php like in symfony-skeleton

--- public/index.php
+++ public/index.php
@@ -1,24 +1,25 @@
 <?php
 
-declare(strict_types=1);
-
+use App\Kernel;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require \dirname(__DIR__).'/vendor/autoload.php';
+require \dirname(__DIR__).'/config/bootstrap.php';
 
-if (\class_exists(Dotenv::class) && \file_exists($env = \dirname(__DIR__).'/.env')) {
-    (new Dotenv())->load($env);
-}
-
-if ($debug = \filter_var(\getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN)) {
+if ($_SERVER['APP_DEBUG']) {
     \umask(0000);
     Debug::enable();
 }
 
-$kernel = new App\Kernel(\getenv('APP_ENV'), $debug);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
 
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
