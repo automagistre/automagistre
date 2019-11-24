2cd84d7e74c1625079c7ab1641d7c1e061ded7c9
Konstantin Grachev <me@grachevko.ru>
2019-11-24 17:22:43 +0300

    Add typehints to Events

--- src/Event/PaymentCreated.php
+++ src/Event/PaymentCreated.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace App\Event;
 
+use App\Entity\Tenant\Transaction;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
+ * @method Transaction getSubject()
+ *
  * @author Konstantin Grachev <me@grachevko.ru>
  */
 final class PaymentCreated extends GenericEvent
 {
+    public function __construct(Transaction $subject, array $arguments = [])
+    {
+        parent::__construct($subject, $arguments);
+    }
 }
