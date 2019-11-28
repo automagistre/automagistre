af377618b15232bdc96ea944697627730b46fa39
Konstantin Grachev <me@grachevko.ru>
2019-11-28 13:06:40 +0300

    Symfony Deprecation: Use Symfony\Component\ErrorHandler\Debug instead of Symfony\Component\Debug\Debug

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
 
@@ -12,6 +13,13 @@ $_SERVER += $_ENV;
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || \filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
+if ($_SERVER['APP_DEBUG']) {
+    \umask(0000);
+    if (\class_exists(Debug::class)) {
+        Debug::enable();
+    }
+}
 
 Premier\Enum\Doctrine\EnumType::register(App\Enum\Carcase::class, 'carcase_enum');
 Premier\Enum\Doctrine\EnumType::register(App\Enum\CarTransmission::class, 'car_transmission_enum');
