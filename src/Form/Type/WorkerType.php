d8d4686bb007f5067f67280f58f8adf4d252d8f0
Konstantin Grachev <me@grachevko.ru>
2020-03-08 01:35:06 +0300

    fix worker type

--- src/Form/Type/WorkerType.php
+++ src/Form/Type/WorkerType.php
@@ -81,18 +81,18 @@ final class WorkerType extends AbstractType
 
         /** @var OrderItemService[] $services */
         $services = $em->createQueryBuilder()
-            ->select('entity')
+            ->select('entity.worker.id')
             ->from(OrderItemService::class, 'entity')
             ->where('entity.createdAt > :today')
             ->andWhere('entity.worker.id IS NOT NULL')
             ->groupBy('entity.worker.id')
             ->setParameter('today', new DateTime('-10 hour'))
             ->getQuery()
-            ->getResult();
+            ->getScalarResult();
 
         $map = [];
         foreach ($services as $service) {
-            $map[$service->getWorker()->getId()] = true;
+            $map[$service['worker.id']] = true;
         }
 
         return $map;
