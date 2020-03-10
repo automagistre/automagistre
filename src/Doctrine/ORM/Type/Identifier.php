fb9952a12602a0a5de9997af200fa505d9874f03
Konstantin Grachev <me@grachevko.ru>
2020-03-11 00:25:44 +0300

    [Feature] Identifier

---
+++ src/Doctrine/ORM/Type/Identifier.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Doctrine\ORM\Type;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+abstract class Identifier
+{
+    private UuidInterface $uuid;
+
+    /**
+     * @final
+     */
+    private function __construct(UuidInterface $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    final public function __toString(): string
+    {
+        return $this->uuid->toString();
+    }
+
+    /**
+     * @return static
+     */
+    final public static function generate(): self
+    {
+        return new static(Uuid::uuid4());
+    }
+
+    /**
+     * @return static
+     */
+    final public static function fromString(string $uuid): self
+    {
+        return new static(Uuid::fromString($uuid));
+    }
+
+    /**
+     * @return static
+     */
+    final public static function fromUuid(UuidInterface $uuid): self
+    {
+        return new static($uuid);
+    }
+
+    final public function toUuid(): UuidInterface
+    {
+        return $this->uuid;
+    }
+}
