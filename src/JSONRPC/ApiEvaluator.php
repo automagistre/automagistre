6816a234c280f381f3adafbbc3046a9359e10977
Konstantin Grachev <me@grachevko.ru>
2020-04-19 16:35:53 +0300

    [Refactoring] JSONRPC Catch NotNormalizableValueException, move ViolationList normalization to CommandArgumentValueResolver

--- src/JSONRPC/ApiEvaluator.php
+++ src/JSONRPC/ApiEvaluator.php
@@ -5,25 +5,20 @@ declare(strict_types=1);
 namespace App\JSONRPC;
 
 use App\ArgumentResolver\ArgumentResolverInterface;
-use function array_map;
 use function assert;
 use Datto\JsonRpc\Evaluator;
 use Datto\JsonRpc\Exceptions\ApplicationException;
-use Datto\JsonRpc\Exceptions\Exception;
 use Datto\JsonRpc\Exceptions\MethodException;
 use function get_class;
 use function is_array;
 use function is_callable;
 use function is_object;
-use function iterator_to_array;
 use LogicException;
 use function method_exists;
 use Pagerfanta\Exception\OutOfRangeCurrentPageException;
 use Psr\Container\ContainerInterface;
 use Sentry\SentryBundle\SentryBundle;
 use function sprintf;
-use Symfony\Component\Validator\ConstraintViolationInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Throwable;
 
 final class ApiEvaluator implements Evaluator
@@ -32,16 +27,10 @@ final class ApiEvaluator implements Evaluator
 
     private ArgumentResolverInterface $argumentResolver;
 
-    private TranslatorInterface $translator;
-
-    public function __construct(
-        ContainerInterface $container,
-        ArgumentResolverInterface $argumentResolver,
-        TranslatorInterface $translator
-    ) {
+    public function __construct(ContainerInterface $container, ArgumentResolverInterface $argumentResolver)
+    {
         $this->container = $container;
         $this->argumentResolver = $argumentResolver;
-        $this->translator = $translator;
     }
 
     /**
@@ -56,20 +45,7 @@ final class ApiEvaluator implements Evaluator
         $callable = $this->container->get($method);
         assert(is_callable($callable));
 
-        try {
-            $args = $this->argumentResolver->getArguments($arguments, $callable);
-        } catch (ValidationException $e) {
-            $errors = array_map(fn (ConstraintViolationInterface $violation) => [
-                'field' => $violation->getPropertyPath(),
-                'message' => $this->translator->trans(
-                    $violation->getMessageTemplate(),
-                    $violation->getParameters(),
-                    'validators'
-                ),
-            ], iterator_to_array($e->violations));
-
-            throw new ArgumentException($errors);
-        }
+        $args = $this->argumentResolver->getArguments($arguments, $callable);
 
         try {
             $response = $callable(...$args);
@@ -83,8 +59,6 @@ final class ApiEvaluator implements Evaluator
             }
 
             throw new LogicException(sprintf('Can\'t normalize response object %s', get_class($response)));
-        } catch (Exception $e) {
-            throw $e;
         } catch (OutOfRangeCurrentPageException $e) {
             throw new ArgumentException(['message' => $e->getMessage()]);
         } catch (Throwable $e) {
--- src/JSONRPC/CommandArgumentValueResolver.php
+++ src/JSONRPC/CommandArgumentValueResolver.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace App\JSONRPC;
 
 use App\ArgumentResolver\ArgumentValueResolverInterface;
+use function array_map;
 use function assert;
 use function class_exists;
 use function count;
 use function in_array;
 use function is_string;
+use function iterator_to_array;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class CommandArgumentValueResolver implements ArgumentValueResolverInterface
 {
@@ -20,10 +25,16 @@ final class CommandArgumentValueResolver implements ArgumentValueResolverInterfa
 
     private ValidatorInterface $validator;
 
-    public function __construct(DenormalizerInterface $denormalizer, ValidatorInterface $validator)
-    {
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        DenormalizerInterface $denormalizer,
+        ValidatorInterface $validator,
+        TranslatorInterface $translator
+    ) {
         $this->denormalizer = $denormalizer;
         $this->validator = $validator;
+        $this->translator = $translator;
     }
 
     /**
@@ -47,11 +58,22 @@ final class CommandArgumentValueResolver implements ArgumentValueResolverInterfa
             'allow_extra_attributes' => true,
         ];
 
-        $dto = $this->denormalizer->denormalize($data, $class, null, $context);
+        try {
+            $dto = $this->denormalizer->denormalize($data, $class, null, $context);
+        } catch (NotNormalizableValueException $e) {
+            throw new ArgumentException(['message' => $e->getMessage()]);
+        }
 
         $errors = $this->validator->validate($dto);
         if (0 !== count($errors)) {
-            throw new ValidationException($errors);
+            throw new ArgumentException(array_map(fn (ConstraintViolationInterface $violation) => [
+                'field' => $violation->getPropertyPath(),
+                'message' => $this->translator->trans(
+                    $violation->getMessageTemplate(),
+                    $violation->getParameters(),
+                    'validators'
+                ),
+            ], iterator_to_array($errors)));
         }
 
         yield $dto;
