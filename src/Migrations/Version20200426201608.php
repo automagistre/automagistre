1abc7dc098e94b92f28a4bbd71968030e6d964c1
Konstantin Grachev <me@grachevko.ru>
2020-04-26 23:38:00 +0300

    [Refactoring] Remove postUp from migrations

--- src/Migrations/Version20200426201608.php
+++ src/Migrations/Version20200426201608.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Migrations;
 
-use function array_map;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use function implode;
-use Ramsey\Uuid\Uuid;
-use function sprintf;
 use function strpos;
 
 final class Version20200426201608 extends AbstractMigration
@@ -23,19 +19,6 @@ final class Version20200426201608 extends AbstractMigration
         $this->addSql('ALTER TABLE car_model ALTER uuid TYPE UUID');
         $this->addSql('ALTER TABLE car_model ALTER uuid DROP DEFAULT');
         $this->addSql('COMMENT ON COLUMN car_model.uuid IS \'(DC2Type:vehicle_id)\'');
-    }
-
-    public function postUp(Schema $schema): void
-    {
-        $conn = $this->connection;
-
-        $stmt = $conn->executeQuery('SELECT id from car_model ORDER BY id');
-        $values = $stmt->fetchAll();
-        if ([] !== $values) {
-            $values = array_map(fn (array $row) => sprintf("(%s, '%s'::uuid)", $row['id'], Uuid::uuid6()->toString()), $values);
-            $values = implode(', ', $values);
-            $conn->executeQuery("UPDATE car_model as t SET uuid = v.uuid FROM (values {$values}) AS v(id, uuid) WHERE t.id = v.id");
-        }
     }
 
     public function down(Schema $schema): void
