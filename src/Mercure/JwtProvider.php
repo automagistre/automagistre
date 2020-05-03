fc050bbd68d4f126e229fe3c714b7a3995b6e18e
Konstantin Grachev <me@grachevko.ru>
2020-05-03 19:35:27 +0300

    fix jwt token

--- src/Mercure/JwtProvider.php
+++ src/Mercure/JwtProvider.php
@@ -10,10 +10,17 @@ use Lcobucci\JWT\Signer\Key;
 
 final class JwtProvider
 {
+    private string $key;
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
     public function __invoke(): string
     {
         return (string) (new Builder())
             ->withClaim('mercure', ['publish' => []])
-            ->getToken(new Sha256(), new Key('aVerySecretKey'));
+            ->getToken(new Sha256(), new Key($this->key));
     }
 }
