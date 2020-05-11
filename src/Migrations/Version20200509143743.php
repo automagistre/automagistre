a2c81643810b066684e7484bf22b400820f12e4c
Konstantin Grachev <me@grachevko.ru>
2020-05-11 15:44:54 +0300

    Version20200509143743 remove data migration

--- src/Migrations/Version20200509143743.php
+++ src/Migrations/Version20200509143743.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Migrations;
 
-use function array_map;
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use function sprintf;
+use LogicException;
 use function strpos;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
@@ -23,26 +21,8 @@ final class Version20200509143743 extends AbstractMigration implements Container
 
         $this->skipIf(0 !== strpos($this->connection->getDatabase(), 'tenant'), 'tenant only');
 
-        $suppliers = $this->connection->fetchAll('SELECT DISTINCT supplier_id FROM income');
-        $suppliers = array_map('array_shift', $suppliers);
-
-        $uuids = $this->container->get('doctrine.dbal.landlord_connection')->fetchAll(
-            'SELECT id, uuid FROM operand WHERE id IN (:ids)',
-            [
-                'ids' => $suppliers,
-            ],
-            [
-                'ids' => Connection::PARAM_INT_ARRAY,
-            ]
-        );
-
-        $this->addSql('ALTER TABLE income ADD supplier_uuid UUID DEFAULT NULL');
-        foreach ($uuids as $uuid) {
-            $this->addSql(sprintf('UPDATE income SET supplier_uuid = \'%s\'::uuid WHERE supplier_id = %s', $uuid['uuid'], $uuid['id']));
-        }
         $this->addSql('ALTER TABLE income DROP supplier_id');
-        $this->addSql('ALTER TABLE income RENAME supplier_uuid TO supplier_id');
-        $this->addSql('ALTER TABLE income ALTER supplier_id SET NOT NULL');
+        $this->addSql('ALTER TABLE income ADD supplier_id UUID NOT NULL ');
         $this->addSql('COMMENT ON COLUMN income.supplier_id IS \'(DC2Type:operand_id)\'');
     }
 
@@ -52,7 +32,6 @@ final class Version20200509143743 extends AbstractMigration implements Container
 
         $this->skipIf(0 !== strpos($this->connection->getDatabase(), 'tenant'), 'tenant only');
 
-        $this->addSql('ALTER TABLE income ADD supplier_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE income DROP supplier');
+        throw new LogicException('Nope.');
     }
 }
