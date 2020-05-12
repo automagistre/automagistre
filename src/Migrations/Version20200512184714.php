27387821822518e3ac75964401e5154afeaf7bfe
Konstantin Grachev <me@grachevko.ru>
2020-05-13 01:57:34 +0300

    Version20200512184714 remove data migration

--- src/Migrations/Version20200512184714.php
+++ src/Migrations/Version20200512184714.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace App\Migrations;
 
-use function array_map;
-use function array_reduce;
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 use LogicException;
-use function sprintf;
 use function strpos;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
@@ -26,31 +22,6 @@ final class Version20200512184714 extends AbstractMigration implements Container
         $this->skipIf(0 !== strpos($this->connection->getDatabase(), 'tenant'), 'tenant only');
 
         $this->addSql('ALTER TABLE order_item_service ADD worker_uuid UUID DEFAULT NULL');
-
-        //> Data migrations
-        /** @var Connection $landlord */
-        $landlord = $this->container->get('doctrine.dbal.landlord_connection');
-        $tenant = $this->connection;
-
-        $operands = $tenant->fetchAll('SELECT DISTINCT worker_id FROM order_item_service');
-        $operands = array_map('array_shift', $operands);
-        $operands = $landlord->fetchAll('SELECT id, uuid FROM operand WHERE id IN (:ids)', ['ids' => $operands], ['ids' => Connection::PARAM_INT_ARRAY]);
-
-        $whenThen = array_reduce(
-            $operands,
-            fn (
-                string $case,
-                array $row
-            ) => $case.sprintf(' WHEN %s = worker_id THEN \'%s\'::uuid', $row['id'], $row['uuid']),
-            ''
-        );
-
-        if ('' !== $whenThen) {
-            $whenThen = 'CASE '.$whenThen.' END';
-            $this->addSql('UPDATE order_item_service SET worker_uuid = '.$whenThen);
-        }
-        //< Data migrations
-
         $this->addSql('ALTER TABLE order_item_service DROP worker_id');
         $this->addSql('ALTER TABLE order_item_service RENAME worker_uuid TO worker_id');
         $this->addSql('COMMENT ON COLUMN order_item_service.worker_id IS \'(DC2Type:operand_id)\'');
