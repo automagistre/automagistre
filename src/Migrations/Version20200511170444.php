e6d0f75de330b1c63d1a870b266514cbbe64ccbe
Konstantin Grachev <me@grachevko.ru>
2020-05-13 01:57:53 +0300

    Version20200511170444 remove data migration

--- src/Migrations/Version20200511170444.php
+++ src/Migrations/Version20200511170444.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Migrations;
 
-use App\Order\Entity\OrderId;
-use function array_map;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use function sprintf;
 use function strpos;
 
 final class Version20200511170444 extends AbstractMigration
@@ -20,20 +17,6 @@ final class Version20200511170444 extends AbstractMigration
         $this->skipIf(0 !== strpos($this->connection->getDatabase(), 'tenant'), 'tenant only');
 
         $this->addSql('ALTER TABLE income_part ADD uuid UUID DEFAULT NULL');
-
-        $ids = $this->connection->fetchAll('SELECT id FROM income_part');
-        /** @var int[] $ids */
-        $ids = array_map('array_shift', $ids);
-        foreach ($ids as $id) {
-            $this->addSql(
-                sprintf(
-                    'UPDATE income_part SET uuid = \'%s\'::uuid WHERE id = %s',
-                    OrderId::generate()->toString(),
-                    $id
-                )
-            );
-        }
-
         $this->addSql('ALTER TABLE income_part ALTER uuid SET NOT NULL ');
         $this->addSql('COMMENT ON COLUMN income_part.uuid IS \'(DC2Type:uuid)\'');
     }
