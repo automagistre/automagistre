2fbd065e6a6d9879389546d88501c83032e84a4c
Konstantin Grachev <me@grachevko.ru>
2020-06-05 02:49:42 +0300

    Trust Proxies, proxy_set_headers

--- public/index.php
+++ public/index.php
@@ -4,9 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
+Request::setTrustedProxies(['REMOTE_ADDR'], Request::HEADER_X_FORWARDED_ALL);
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
