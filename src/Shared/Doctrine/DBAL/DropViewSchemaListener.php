043f333a8e6493f1eb4774271d6a76b766da5423
Konstantin Grachev <me@grachevko.ru>
2020-06-21 16:03:54 +0300

    Drop all view tables that have _view suffix

--- src/Shared/Doctrine/DBAL/DropViewSchemaListener.php
+++ src/Shared/Doctrine/DBAL/DropViewSchemaListener.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace App\Shared\Doctrine\DBAL;
 
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use function str_ends_with;
 
 final class DropViewSchemaListener
 {
-    private const VIEWS = [
-        'calendar_entry_view',
-        'warehouse_view',
-    ];
-
     public function postGenerateSchema(GenerateSchemaEventArgs $args): void
     {
         $schema = $args->getSchema();
 
-        foreach (self::VIEWS as $view) {
-            if ($schema->hasTable($view)) {
-                $schema->dropTable($view);
+        foreach ($schema->getTables() as $table) {
+            if (str_ends_with($table->getName(), '_view')) {
+                $schema->dropTable($table->getName());
             }
         }
     }
