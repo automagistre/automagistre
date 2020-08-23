ce04328a3ebde809deb54ff32ddde7591ee5b2f4
Konstantin Grachev <me@grachevko.ru>
2020-08-23 14:47:00 +0300

    Make Manufacturer unique by name

--- src/Manufacturer/Entity/Manufacturer.php
+++ src/Manufacturer/Entity/Manufacturer.php
@@ -19,7 +19,7 @@ class Manufacturer
     public ManufacturerId $id;
 
     /**
-     * @ORM\Column(name="name", length=64, nullable=true)
+     * @ORM\Column(name="name", length=64, unique=true)
      */
     private ?string $name;
 
