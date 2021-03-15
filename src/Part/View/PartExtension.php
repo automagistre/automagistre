54c5bf1ae5035d6ac312f61622890f785a406e4a
Konstantin Grachev <me@grachevko.ru>
2021-03-15 14:50:16 +0300

    Part: Add reserved to PartView

--- src/Part/View/PartExtension.php
+++ src/Part/View/PartExtension.php
@@ -40,14 +40,9 @@ final class PartExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('part_in_stock', fn (Part $part): int => $this->partManager->inStock($part->toId())),
-            new TwigFunction('part_crosses', fn (Part $part): array => $this->partManager->getCrosses($part->toId())),
             new TwigFunction('part_crosses_in_stock', fn (
-                Part $part
-            ): array => $this->partManager->crossesInStock($part->toId())),
-            new TwigFunction('part_reserved', fn (
-                Part $part
-            ): int => $this->reservationManager->reserved($part->toId())),
+                PartId $partId
+            ): array => $this->partManager->crossesInStock($partId)),
             new TwigFunction('part_reserved_in_item', fn (
                 OrderItemPart $part
             ): int => $this->reservationManager->reserved($part)),
