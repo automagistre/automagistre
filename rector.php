7662b6a4c5e173a3c9f53b9531ddc2fe252b3327
Konstantin Grachev <me@grachevko.ru>
2021-05-09 14:30:56 +0300

    Install rector

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+    $parameters->set(Option::ENABLE_CACHE, true);
+    $parameters->set(Option::CACHE_DIR, __DIR__.'/var/rector');
+    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__.'/var/cache/test/App_KernelTestDebugContainer.xml');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(Rector\Restoration\Rector\Namespace_\CompleteImportForPartialAnnotationRector::class)
+        ->call('configure', [
+            [
+                Rector\Restoration\Rector\Namespace_\CompleteImportForPartialAnnotationRector::USE_IMPORTS_TO_RESTORE => ValueObjectInliner::inline([
+                    new Rector\Restoration\ValueObject\CompleteImportForPartialAnnotation('Doctrine\ORM\Mapping', 'ORM'),
+                ]),
+            ],
+        ])
+    ;
+};
