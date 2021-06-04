668d626a113b7c154eac8e4c51b3222fd45eb404
Konstantin Grachev <me@grachevko.ru>
2021-06-04 19:56:41 +0300

    Bump doctrine with dependencies

---
+++ src/Migrations/Version20210604165242.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20210604165242 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE car_recommendation_part ALTER recommendation_id DROP NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE car_recommendation_part ALTER recommendation_id SET NOT NULL');
+    }
+}
