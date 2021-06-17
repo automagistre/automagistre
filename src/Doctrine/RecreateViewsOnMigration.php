f561f76fbfd96c18f125392717380fc7db352720
Konstantin Grachev <me@grachevko.ru>
2021-06-17 20:55:38 +0300

    Fix: All views must be dropped on migration, not only present in files

--- src/Doctrine/RecreateViewsOnMigration.php
+++ src/Doctrine/RecreateViewsOnMigration.php
@@ -8,9 +8,9 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\Migrations\Event\MigrationsEventArgs;
 use Doctrine\Migrations\Events;
 use Symfony\Component\Finder\Finder;
+use function Safe\file_get_contents;
 use function sprintf;
 use function Symfony\Component\String\u;
-use function Safe\file_get_contents;
 
 final class RecreateViewsOnMigration implements EventSubscriber
 {
@@ -29,24 +29,14 @@ final class RecreateViewsOnMigration implements EventSubscriber
         ];
     }
 
-    /**
-     * Кеш чтобы не бегать дважды по файлам.
-     */
-    private array $views = [];
-
-    /**
-     * {@inheritdoc}
-     */
     public function onMigrationsMigrating(MigrationsEventArgs $event): void
     {
         $conn = $event->getConnection();
 
-        foreach ((new Finder())->in($this->path)->files() as $file) {
-            $name = u($file->getBasename())->replace('.sql', '_view')->snake()->toString();
+        $views = $conn->fetchFirstColumn("SELECT table_name FROM information_schema.views WHERE table_name ~ '_view$'");
 
-            $conn->executeQuery(sprintf('DROP VIEW IF EXISTS %s', $name));
-
-            $this->views[$name] = $file->getRealPath();
+        foreach ($views as $view) {
+            $conn->executeQuery(sprintf('DROP VIEW %s', $view));
         }
     }
 
@@ -54,8 +44,10 @@ final class RecreateViewsOnMigration implements EventSubscriber
     {
         $conn = $event->getConnection();
 
-        foreach ($this->views as $name => $file) {
-            $conn->executeQuery(sprintf('CREATE VIEW %s AS %s', $name, file_get_contents($file)));
+        foreach ((new Finder())->in($this->path)->files() as $file) {
+            $name = u($file->getBasename())->replace('.sql', '_view')->snake()->toString();
+
+            $conn->executeQuery(sprintf('CREATE VIEW %s AS %s', $name, file_get_contents($file->getPathname())));
         }
     }
 }
