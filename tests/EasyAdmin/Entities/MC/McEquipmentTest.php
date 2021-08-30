61945ca68205053287457a7f727c4cb62d439511
Konstantin Grachev <me@grachevko.ru>
2021-08-30 12:59:03 +0300

    McEquipment: allow show

--- tests/EasyAdmin/Entities/MC/McEquipmentTest.php
+++ tests/EasyAdmin/Entities/MC/McEquipmentTest.php
@@ -4,10 +4,30 @@ declare(strict_types=1);
 
 namespace App\Tests\EasyAdmin\Entities\MC;
 
+use App\Fixtures\Mc\EquipmentFixtures;
 use App\Tests\EasyAdminTestCase;
+use function http_build_query;
 
 final class McEquipmentTest extends EasyAdminTestCase
 {
+    /**
+     * @see \App\MC\Controller\EquipmentController::showAction()
+     */
+    public function testShow(): void
+    {
+        $client = self::createClient();
+
+        $client->request('GET', '/msk/?'.http_build_query([
+            'action' => 'show',
+            'entity' => 'McEquipment',
+            'id' => EquipmentFixtures::ID,
+        ]));
+
+        $response = $client->getResponse();
+
+        self::assertSame(200, $response->getStatusCode());
+    }
+
     /**
      * @see \App\MC\Controller\EquipmentController::listAction()
      */
