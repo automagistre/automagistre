786aac59a8e6ef3108e7aca906b347576bec85bb
Konstantin Grachev <me@grachevko.ru>
2021-09-12 17:48:44 +0300

    Mark IncomeAccrued event as Async

--- src/Income/Event/IncomeAccrued.php
+++ src/Income/Event/IncomeAccrued.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Income\Event;
 
 use App\Income\Entity\IncomeId;
+use App\MessageBus\Async;
 
 /**
  * @psalm-immutable
  */
-final class IncomeAccrued
+final class IncomeAccrued implements Async
 {
     public function __construct(public IncomeId $incomeId)
     {
