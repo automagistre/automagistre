63f8ccb3bae47863842a77f5e949059898bc2b67
Konstantin Grachev <me@grachevko.ru>
2021-10-17 15:58:48 +0300

    Use find in `part_view` function to reduce calls to database

--- src/Part/View/PartExtension.php
+++ src/Part/View/PartExtension.php
@@ -14,6 +14,7 @@ use App\Part\Manager\PartManager;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
+use LogicException;
 use function number_format;
 
 /**
@@ -46,7 +47,7 @@ final class PartExtension extends AbstractExtension
             new TwigFunction('part_by_id', fn (PartId $partId): Part => $this->partManager->byId($partId)),
             new TwigFunction(
                 'part_view',
-                fn (PartId $partId): PartView => $this->registry->getBy(PartView::class, ['id' => $partId]),
+                fn (PartId $partId): PartView => $this->registry->find(PartView::class, $partId) ?? throw new LogicException('Part not found.'),
             ),
         ];
     }
