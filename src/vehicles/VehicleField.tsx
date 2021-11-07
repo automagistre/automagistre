f39b35a931ead4a83e27bef0464b4226720ff647
Konstantin Grachev <me@grachevko.ru>
2021-11-08 01:15:36 +0300

    Vehicle field simplify

--- src/vehicles/VehicleField.tsx
+++ src/vehicles/VehicleField.tsx
@@ -1,16 +1,16 @@
-import {TextField} from 'react-admin'
 import {Vehicle} from '../types'
 import VehicleBodyReferenceField from '../vehicle_bodies/VehicleBodyReferenceField'
 
-const VehicleBodyNameField = ({record}: { record?: Vehicle }) => (
-    <>
-        <VehicleBodyReferenceField record={record} link={false}/>
-        {' '}
-        |
-        {' '}
-        <TextField source="legal_plate" record={record}/>
-    </>
-)
+const VehicleBodyNameField = ({record}: { record?: Vehicle }) => {
+    if (!record) return null
+
+    return (
+        <>
+            <VehicleBodyReferenceField record={record} link={false}/>
+            {record.legal_plate && ` | ${record.legal_plate}`}
+        </>
+    )
+}
 
 VehicleBodyNameField.defaultProps = {
     source: 'vehicle_body',
