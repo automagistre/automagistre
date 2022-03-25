86c8c3135a1db362f421b0e161159cf90f217505
Konstantin Grachev <me@grachevko.ru>
2022-03-25 13:15:22 +0300

    Add var/cache/test/Symfony/Config to phpstan and psalm

--- config/packages/prometheus.php
+++ config/packages/prometheus.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Config\ArtprimaPrometheusMetricsConfig;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 return static function (ArtprimaPrometheusMetricsConfig $prometheus, ContainerConfigurator $configurator): void {
     $prometheus->namespace('automagistre')
@@ -16,8 +17,8 @@ return static function (ArtprimaPrometheusMetricsConfig $prometheus, ContainerCo
     } else {
         $prometheus->type('redis')
             ->redis()
-            ->host('%env(REDIS_HOST)%')
-            ->port('%env(int:REDIS_PORT)%')
+            ->host(env('REDIS_HOST'))
+            ->port(env('int:REDIS_PORT'))
             ->database(1)
         ;
     }
