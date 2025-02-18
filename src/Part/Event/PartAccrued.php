d6886c7814cd91f7a4a038c3fb768b802d212f40
Konstantin Grachev <me@grachevko.ru>
2025-02-18 13:47:27 +0300

    chore: PartAccrued to async

--- src/Part/Event/PartAccrued.php
+++ src/Part/Event/PartAccrued.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Part\Event;
 
 use App\Part\Entity\PartId;
+use App\MessageBus\Async;
 
 /**
  * @psalm-immutable
  */
-final class PartAccrued
+final class PartAccrued implements Async
 {
     public function __construct(public PartId $partId)
     {
