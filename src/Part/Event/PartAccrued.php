bdf215f4c511cad4f4c9837afe795655a66a4875
Konstantin Grachev <me@grachevko.ru>
2025-02-19 13:12:13 +0300

    Revert "chore: PartAccrued to async"
    This reverts commit 7b367b3b268a05f80df040e3275daf033aac7757.

--- src/Part/Event/PartAccrued.php
+++ src/Part/Event/PartAccrued.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace App\Part\Event;
 
 use App\Part\Entity\PartId;
-use App\MessageBus\Async;
 
 /**
  * @psalm-immutable
  */
-final class PartAccrued implements Async
+final class PartAccrued
 {
     public function __construct(public PartId $partId)
     {
